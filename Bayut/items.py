79d190a45c36b849de29ce8050f09bc80a3feadd
AnjanaMariyan <anjanamariyan@gmail.com>
2024-07-02 20:25:13 +0530

    fifth commit

--- Bayut/items.py
+++ Bayut/items.py
@@ -6,7 +6,22 @@
 import scrapy
 
 
-class BayutScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Assignment2Item(scrapy.Item):
+    property_id = scrapy.Field()
+    purpose = scrapy.Field()
+    type_ = scrapy.Field()
+    added_on = scrapy.Field()
+    furnishing = scrapy.Field()
+    currency = scrapy.Field()
+    price = scrapy.Field()
+    location = scrapy.Field()
+    bed = scrapy.Field()
+    bath = scrapy.Field()
+    size = scrapy.Field()
+    permit_number = scrapy.Field()
+    agent_name = scrapy.Field()
+    image_url = scrapy.Field()
+    breadcrumb = scrapy.Field()
+    amenities = scrapy.Field()
+    description = scrapy.Field()
+
--- Bayut/spiders/bayut.py
+++ Bayut/spiders/bayut.py
@@ -1,42 +1,51 @@
 import scrapy
 import re
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from parsel import Selector
-import requests
 
-
-class BayutSpider(scrapy.Spider):
+class BayutSpider(CrawlSpider):
     name = 'bayut'
+    allowed_domains = ['bayut.com']
     start_urls = ['https://www.bayut.com/to-rent/property/dubai/']
+    base_url = 'https://www.bayut.com/s/upgraded-properties-rent-dubai/page-{page_number}'
+    page_number = 1
+    max_pages = 1500
+    max_items = 1000 
+
     
+    scraped_items = 0
+
+    rules = (
+        Rule(LinkExtractor(allow=r'/property/details-\d+\.html'), callback='parse_property'),
+        Rule(LinkExtractor(restrict_css='a._95dd93c1[title="Next"]'), follow=True),
+    )
 
     def parse(self, response):
-        response=requests.get()
         selector = Selector(response.text)
         
+       
         property_urls = selector.xpath("//a[@aria-label='Listing']/@href").getall()
         for url in property_urls:
             yield scrapy.Request(url, callback=self.parse_property)
             
-        page_number = 1
-        max_pages = 200
-
         
-    
-
-        while page_number <= max_pages:
-            url = f'https://www.bayut.com/s/upgraded-properties-rent-dubai/page-{page_number}'
-            print(url)
+        while self.page_number <= self.max_pages and self.scraped_items < self.max_items:
+            url = self.base_url.format(page_number=self.page_number)
             yield scrapy.Request(url, callback=self.parse_next_page)
-            page_number += 1
-
+            self.page_number += 1
 
     def parse_property(self, response):
+        if self.scraped_items >= self.max_items:
+            return
+        
         selector = Selector(response.text)
-
+        
+        
         script_texts = selector.xpath("//script/text()").getall()
         all_script_text = "\n".join(script_texts)
         pattern = r'"permitNumber":"(\d+)"'
-
+        
         property_id = selector.xpath("//span[@aria-label='Reference']/text()").get()
         purpose = selector.xpath("//span[@aria-label='Purpose']/text()").get()
         type_ = selector.xpath("//span[@aria-label='Type']/text()").get()
@@ -52,33 +61,35 @@ class BayutSpider(scrapy.Spider):
         permit_number = permit_numbers[0] if permit_numbers else None
         agent_name = selector.xpath('//span//a[@aria-label="Agent name"]/text()').get()
         image_url = selector.xpath("//img[@aria-label='Cover Photo']/@src").get()
-        breadcrumb = type_ +" " +"for rent in " + '>'.join(selector.xpath('//div[@aria-label="Breadcrumb"]//span[@class="_43ad44d9"]/text()').extract())
-        amenities = selector.xpath('//div[@id="property-amenity-dialog"]//div[@class="_682538c2"]//span[@class="_7181e5ac"]/text()').getall()
-        description = selector.xpath('//div[@aria-label="Property description"]//span[@class="_3547dac9"]//text()').getall()
+        breadcrumb = type_ + " for rent in " + '>'.join(selector.xpath('//div[@aria-label="Breadcrumb"]//span[@class="_43ad44d9"]/text()').extract())
+        amenities = selector.xpath('//div[@class="_91c991df"]//span[@class="_7181e5ac"]/text()').getall()
+        description = ' '.join(selector.xpath('//div[@aria-label="Property description"]//span[@class="_3547dac9"]//text()').getall())
+        
 
-    
-        property_data = {
-            "property_id": property_id,
-            "purpose": purpose,
-            "type": type_,
-            "added_on": added_on,
-            "furnishing": furnishing,
-            "price": {
-                "currency": currency,
-                "amount": price
-            },
-            "location": location,
-            "bed_bath_size": {
-                "bedrooms": bed,
-                "bathrooms": bath,
-                "size": size
-            },
-            "permit_number": permit_number,
-            "agent_name": agent_name,
-            "image_url": image_url,
-            "breadcrumbs": breadcrumb,
-            "amenities": amenities,
-            "description": " ".join(description)
+        self.scraped_items += 1
+        yield {
+            'property_id': property_id,
+            'purpose': purpose,
+            'type': type_,
+            'added_on': added_on,
+            'furnishing': furnishing,
+            'currency': currency,
+            'price': price,
+            'location': location,
+            'bed': bed,
+            'bath': bath,
+            'size': size,
+            'permit_number': permit_number,
+            'agent_name': agent_name,
+            'image_url': image_url,
+            'breadcrumb': breadcrumb,
+            'amenities': amenities,
+            'description': description
         }
 
-        yield property_data
+    def parse_next_page(self, response):
+        if self.scraped_items >= self.max_items:
+            return
+        
+        
+        pass
